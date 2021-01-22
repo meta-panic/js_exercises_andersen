1eac0dfa70cd06552372c7e4511e694e62508072
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-22 14:02:40 +0300

    completed task 2

--- src/task_2.js
+++ src/task_2.js
@@ -4,6 +4,16 @@
  */
 // .. ваш код для filter
 var arr = [1, 2, 3, 4, 5, 6, 7];
+
+/**
+ * Функция принимает в себя массив и другую функцию, возвращает те элементы массива, для которых функция callback возвращает true
+ * @param {*} arr
+ * @param {*} callback
+ */
+function filter(arr, callback) {
+  return arr.filter(callback);
+}
+
 console.log(
   filter(arr, function (a) {
     return a % 2 == 0;
