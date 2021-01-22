9e93633e5dfe52e58d57e3610a0471e49f5cf2bc
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-22 20:20:00 +0300

    optimized code and fixed test

--- src/task_1.js
+++ src/task_1.js
@@ -12,48 +12,33 @@
  */
 
 /**
- * Функция, которая копирует свойства из одного объекта в другой
+ * Функция копирует свойства из целевого объекта и добавляет новое свойство к нему
  * @param {*} inputObject целевой объект
+ * @param {*} prop новое свойство
+ * @param {*} value значение нового свойства
  */
-const copyProp = (inputObject) => Object.assign({}, inputObject);
-
-/**
- * Добавляет свойство passengers
- * @param {*} inputObject целевой объект
- * @param {*} value значение для свойства passengers
- */
-const changeCar = (inputObject, value = 35) => {
-  let obj = copyProp(inputObject);
-  obj.passengers = value;
+const addProp = (inputObject, prop = "default", value = 0) => {
+  let obj = Object.assign({}, inputObject);
+  obj[prop] = value;
   return obj;
 };
-
-/**
- * Функция для объекта типа autoTrack, копирует свойства из целевого объекта и доваляет ему свойство tonnage (значение указываем в параметрах)
- * @param {*} inputObject целевой объект
- * @param {*} value значение для свойства tonnage
- */
-const addTonnage = (inputObject, value = 50) =>
-  Object.assign(copyProp(inputObject), { tonnage: value });
 
 let car = { massa: 1 };
 console.log("Объект car");
 console.log(car);
 
-let bus = changeCar(car, 32);
+let bus = addProp(car, "passengers", 32);
 console.log("Объект bus");
 console.log(bus);
 
 console.log("Объект car");
 console.log(car);
 
-let autoTruck = addTonnage(car, 100);
+let autoTruck = addProp(car, "tonnage", 100);
 console.log("Объект autoTruck");
 console.log(autoTruck);
 
 console.log("Объект car");
 console.log(car);
 
-module.exports.copyProp = copyProp;
-module.exports.changeCar = changeCar;
-module.exports.addTonnage = addTonnage;
+module.exports.addProp = addProp;
--- src/task_1.test.js
+++ src/task_1.test.js
@@ -1,18 +1,11 @@
-const { addTonnage, changeCar, copyProp } = require("./task_1");
+const { addProp } = require("./task_1");
 
-test("Функция копирует свойства объекта и записывает его в новый и возвращает его", () => {
-  expect(copyProp({ massa: 1 })).toEqual({ massa: 1 });
-  expect(copyProp({ prop1: 1, prop2: 2, prop3: null })).toEqual({
+test("Функция копирует свойства из целевого объекта и добавляет новое свойство к нему", () => {
+  expect(addProp({ massa: 1 })).toEqual({ massa: 1, default: 0 });
+  expect(addProp({ prop1: 1, prop2: 2, prop3: null }, "prop4", 100)).toEqual({
     prop1: 1,
     prop2: 2,
     prop3: null,
+    prop4: 100,
   });
-});
-
-test("Функция должна добавлять свойство passengers (значение передается в параметрах, по дефлоту - 35)", () => {
-  expect(changeCar({ prop1: 1 }, 22)).toEqual({ prop1: 1, passengers: 22 });
-});
-
-test("копирует свойства из целевого объекта и доваляет ему свойство tonnage (значение указываем в параметрах, по дефолту - 50)", () => {
-  expect(addTonnage({ prop1: 1 }, 22)).toEqual({ prop1: 1, tonnage: 22 });
 });
