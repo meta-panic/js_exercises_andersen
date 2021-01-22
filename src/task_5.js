76062c1f1bacb3013be8173479353b5e84a110ee
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-22 14:16:32 +0300

    completed task 5

--- src/task_5.js
+++ src/task_5.js
@@ -1,1 +1,7 @@
 // Переделайте решение предыдущей задачи(task_4.js) так, чтобы вместо метода call() был метод apply().
+
+let phrase = { value: "привет" };
+function func(surname, name) {
+  console.log(`${this.value}, ${surname} ${name}`);
+}
+func.apply(phrase, ["Иванов", "Иван"]); //тут должно вывести 'привет, Иванов Иван'
