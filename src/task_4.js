4b8cb9db18698036b67f4c834684009b6430ef03
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-22 14:13:19 +0300

    completed task 4

--- src/task_4.js
+++ src/task_4.js
@@ -7,4 +7,4 @@ let phrase = { value: "привет" };
 function func(surname, name) {
   console.log(`${this.value}, ${surname} ${name}`);
 }
-func(); //тут должно вывести 'привет, Иванов Иван'
+func.call(phrase, "Иванов", "Иван"); //тут должно вывести 'привет, Иванов Иван'
