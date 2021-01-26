ad844ec0399fe2e721ba9067d8d6bc2972dfceaf
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-26 14:53:04 +0300

    add new test for task_3

--- src/test/task_3.test.js
+++ src/test/task_3.test.js
@@ -1,4 +1,4 @@
-const { func } = require("./task_3");
+const { func } = require("../task_3");
 
 test("Используя метод call(), нужно привязать this в функции func к объекту obj", () => {
   let obj = { a: 13, b: 22 };
