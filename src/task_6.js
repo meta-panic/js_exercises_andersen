a70ed7223fbcbb905ded38f85ce43da55c9a6add
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-26 15:57:07 +0300

    fixed the function according to the condition

--- src/task_6.js
+++ src/task_6.js
@@ -4,13 +4,12 @@
  */
 let phrase = { value: "привет" };
 function func(surname, name) {
-  return function () {
-    console.log(`${this.value}, ${surname} ${name}`);
-    return `${this.value}, ${surname} ${name}`;
-  }.bind(phrase)();
+  console.log(`${this.value}, ${surname} ${name}`);
+  return `${this.value}, ${surname} ${name}`;
 } // Тут воспользуйтесь bind()func('Иванов', 'Иван'); //тут должно вывести 'привет, Иванов Иван'func('Петров', 'Петр'); //тут должно вывести 'привет, Петров Петр'
 
-func("Иванов", "Иван");
-func("Петров", "Петр");
+let fn = func.bind(phrase);
+fn("Иванов", "Иван");
+fn("Петров", "Петр");
 
 module.exports.func = func;
