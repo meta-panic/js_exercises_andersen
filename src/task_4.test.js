ebfc6827e60ff03fdc85b4a2801ce5e1422f507b
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-26 15:16:18 +0300

    remove old test and fix path to the index.js

--- src/task_4.test.js
+++
@@ -1,7 +0,0 @@
-const { func } = require("./task_4");
-
-test("Вызов функции func должен выводить на экран фразу 'привет, аргумент1 агрумент2'", () => {
-  let name = "Иван",
-    surname = "Иванов";
-  expect(func(surname, name)).toEqual(`привет, ${surname} ${name}`);
-});
