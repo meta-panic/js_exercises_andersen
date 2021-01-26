bc5d17975bc47b52229e6c0b340cf5ae043b5269
ext.msokolova <ext.msokolova@tcsbank.ru>
2021-01-26 17:30:26 +0300

    Added new tasks

---
+++ src/task_11.js
@@ -0,0 +1,8 @@
+/* Напиши функцию delay(ms), которая возвращает промис,
+ * переходящий в состояние "resolved" через ms миллисекунд и который
+ * несёт значение 100
+ * */
+
+function delay(ms) {}
+
+delay(1000).then((value) => console.log(`Done with ${value}`)); //Done with 100
