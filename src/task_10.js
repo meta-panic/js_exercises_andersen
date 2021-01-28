2c13c91cf605b82afa2257fefb71d857cafa9059
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-28 20:46:40 +0300

    improved the readability of the code

--- src/task_10.js
+++ src/task_10.js
@@ -6,7 +6,7 @@
 let s = [23, 65, 98, 5];
 Array.prototype.myFilter = function (callback) {
   let newArray = []; // Добавьте свой код здесь
-  this.forEach((value) => (callback(value) ? newArray.push(value) : 0));
+  this.forEach((value) => callback(value) && newArray.push(value));
   return newArray;
 };
 let new_s = s.myFilter(function (item) {
