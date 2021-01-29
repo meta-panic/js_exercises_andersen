5eaf265df119611f718f8fdd3ec7c01eb356a25a
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-29 13:12:18 +0300

    complete task_15

--- src/task_15.js
+++ src/task_15.js
@@ -1,4 +1,19 @@
 /** При помощи функции run реализовать task_14 на ES7 стандарте
  */
-async function run() {}
-run();
+
+async function run() {
+  let value;
+  try {
+    value = await Promise.resolve(3400);
+    if (value > 300) {
+      throw new Error("Error");
+    }
+    return "Horay";
+  } catch (e) {
+    console.log(e);
+  } finally {
+    console.log("This is Finally!");
+  }
+}
+
+console.log(run());
