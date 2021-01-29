d58a148dfb3a0419b7e4bdf740f7866eef6d01c6
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-29 12:50:43 +0300

    complete task_13

--- src/task_13.js
+++ src/task_13.js
@@ -4,3 +4,38 @@
  * если значение равно true получи и выведи в консоль
  * данные из http://www.json-generator.com/api/json/get/cfVGucaXPC
  */
+
+function getData(url = "http://www.json-generator.com/api/json/get/cfQCylRjuG") {
+  fetch(url)
+    .then(
+      (response) => response.json(),
+      (err) => {
+        throw new Error(err);
+      }
+    )
+    .then(
+      (response) => console.log(response.getUsersData),
+      (err) => {
+        console.log(err);
+      }
+    );
+}
+
+getData();
+
+// Вариант 2
+
+function getData2(url = "http://www.json-generator.com/api/json/get/cfQCylRjuG") {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onload = () => resolve(JSON.parse(xhr.responseText));
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send();
+  }).then(
+    (resolve) => console.log(resolve.getUsersData),
+    (reject) => console.log(reject)
+  );
+}
+
+getData2();
